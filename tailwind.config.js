67b3e5fbef6f8b0f2ed5837cb5413ec0967f2c48
ihojmanb <ihojmanb@gmail.com>
2021-09-29 00:00:25 -0300

    purge tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,52 @@
+// let remark = require('remark')
 module.exports = {
-    future: {
-      // removeDeprecatedGapUtilities: true,
-      // purgeLayersByDefault: true,
-    },
-    // mode: "jit",
-    purge: [
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  // mode: "jit",
+  purge: {
+    enabled: true,
+    content: [
       "./.eleventy.js",
-      "./public/**/*.html",
       "./src/_includes/**/*.njk",
-      "./src/posts/**/*.md",
-      // "./src/scripts/**/*.js",
-      // "./src/styles/**/*.css",
+      "./src/_layouts/**/*.njk",
+      "./src/_books/**/*.njk",
+      "./src/_books/**/*.json",
+      "./src/pages/**/*.{md,njk,json}",
+      "./src/scripts/**/*.js",
+      "./src/styles/**/*.css",
       "./src/tests/**/*.js",
+      "./src/writing/**/*.md",
+      "./src/writing/**/*.json",
       "./src/index.njk",
-      "./src/About.md",
       "./src/**/*.js",
     ],
-    theme: {
-      extend: {
-        fontFamily: {
-          body: ["IBM Plex Sans"],
-        },
+    // transform: {
+    //   md: (content) => {
+    //     return remark().proces(content)
+    //   }
+    // }
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["IBM Plex Sans"],
       },
     },
-    variants: {},
-    plugins: [
-      require('@tailwindcss/typography'),
-      require('tailwind-underline-utils'),
-      function ({ addUtilities }) {
-        const extendUnderline = {
-          ".underline-magenta": {
-            textDecoration: "underline",
-            "text-decoration-color": "#FF00FF",
-          },
-        };
-        addUtilities(extendUnderline);
-      },
-    ],
-  };
-  
+  },
+  variants: {},
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwind-underline-utils"),
+    function ({ addUtilities }) {
+      const extendUnderline = {
+        ".underline-magenta": {
+          textDecoration: "underline",
+          "text-decoration-color": "#FF00FF",
+        },
+      };
+      addUtilities(extendUnderline);
+    },
+  ],
+};
